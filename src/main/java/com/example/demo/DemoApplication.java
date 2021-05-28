62b9dce20ca15f83fb9a952f5c632e3db2477e62
JanMedyk <jan.medyk97@gmail.com>
2021-05-28 18:42:46 +0200

    aaaa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 
 
-@SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
