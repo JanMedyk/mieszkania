d67a3868f2a08a9a6e9a2f5103f146c8c3cb1971
JanMedyk <jan.medyk97@gmail.com>
2021-05-28 18:59:43 +0200

    aascxzxz

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.management.Query;
@@ -19,14 +18,13 @@ public class aa {
     MieszkanieRepository mieszkaniaRepository;
 
     @RequestMapping("/")
-    @ResponseBody
-    public String home() {
 
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("home");
+    public ModelAndView home() {
 
-//        return modelAndView;
-        return "Hello";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        return modelAndView;
     }
 
 
