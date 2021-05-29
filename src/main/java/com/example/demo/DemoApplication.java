ecad3e43b62c4f5fa2adfe6fdb72d0608efa0ca4
JanMedyk <jan.medyk97@gmail.com>
2021-05-29 19:53:52 +0200

    xz

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan
 
 
